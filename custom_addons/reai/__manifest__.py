36fd4b8c6c1a86cd9caa627869df5158708e4c47
Mateo Kocev <mkocev@student.unipu.hr>
2025-03-14 00:10:36 +0100

    Initial Setup
    Initial setup for the custom addon, refactoring the directory to fix the error.

---
+++ custom_addons/reai/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Reai',
+    'version': '0.1',
+    'summary': 'Testing version of the REAI addon',
+    'author': 'Mateo K.',
+    'depends': ['base', 'crm'],
+    'data': [
+        'security/ir.model.access.*',
+        'views/reai_view.xml'
+    ],
+    'installable': True,
+    'application': False,
+}
